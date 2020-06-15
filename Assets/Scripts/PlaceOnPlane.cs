8a80475d792fe84aff70d77e2effa2d35f998a52
Ben Beverly <bbeverl@gmail.com>
2020-06-15 13:42:10 -0400

    Add demo experience files

---
+++ Assets/Scripts/PlaceOnPlane.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+/// <summary>
+/// Listens for touch events to create objects on a AR Plane based on AR Raycast
+/// Originally take form Unity Technologies AR Foundation samples
+/// <seealso cref="https://github.com/Unity-Technologies/arfoundation-samples/blob/master/Assets/Scripts/PlaceOnPlane.cs"/>
+///
+/// Will also perform a raycast and tell the object to spin <seealso cref="Spin"/>
+/// </summary>
+public class PlaceOnPlane : MonoBehaviour
+{
+    [SerializeField]
+    private ARRaycastManager raycastManager = null;
+
+    [SerializeField]
+    private GameObject prefabToPlace = null;
+
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (raycastManager == null)
+        {
+            raycastManager = GetComponent<ARRaycastManager>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (SpinCubes(touch) == false)
+                {
+                    CreateCubes(touch);
+                }
+            }
+        }
+    }
+
+    private void CreateCubes(Touch touch)
+    {
+        if (raycastManager.Raycast(touch.position, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+        {
+            Pose post = hits[0].pose;
+            Instantiate(prefabToPlace, post.position, post.rotation);
+        }
+    }
+
+    private bool SpinCubes(Touch touch)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(touch.position);
+
+        if (Physics.Raycast(ray, out RaycastHit hitInfo))
+        {
+            Debug.Log("Clicked " + hitInfo.transform.gameObject.name);
+            if (hitInfo.transform.TryGetComponent(out Spin spin))
+            {
+                spin.StartSpin();
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Spins the object for 1 second
+/// </summary>
+public class Spin : MonoBehaviour
+{
+    public void StartSpin()
+    {
+        StartCoroutine(Rotate());
+    }
+
+    private IEnumerator Rotate(float duration = 1)
+    {
+        float startRotation = transform.eulerAngles.y;
+        float endRotation = startRotation + 360.0f;
+        float deltaTime = 0.0f;
+        while (deltaTime < duration)
+        {
+            deltaTime += Time.deltaTime;
+            float yRotation = Mathf.Lerp(startRotation, endRotation, deltaTime / duration) % 360.0f;
+            transform.eulerAngles = new Vector3(
+                transform.eulerAngles.x,
+                yRotation,
+                transform.eulerAngles.z);
+
+            yield return null;
+        }
+    }
+}
